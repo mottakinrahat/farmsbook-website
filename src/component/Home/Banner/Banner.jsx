4c434ce1fba6e22c24ede784955e40b959a99388
mottakinrahat <rahatchowhdury661@gmail.com>
2023-08-08 20:58:17 +0600

    LinkAdded

--- src/component/Home/Banner/Banner.jsx
+++ src/component/Home/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './BannerCss/banner.css'
+import { Link } from 'react-router-dom';
 const Banner = () => {
     const bannerImgUrl = 'https://i.ibb.co/ZS568Bt/2nd.png';
     return (
@@ -17,8 +18,8 @@ const Banner = () => {
                             Streamline harvest management.
                             Unlock demand forecasting's full potential.</p>
                         <div className='flex  md:gap-[80px] gap-10  md:ml-[240px] ml-[50px]'>
-                            <button className='md:px-[16px]   md:py-[16px] border-4 font-semibold py-[4px] px-[8px] border-white rounded-full'>Download App</button>
-                            <button className='md:px-[49px] md:py-[16px] border-4 py-[4px] px-[20px] font-bold border-white text-black bg-white rounded-full flex gap-1'>Join Us</button>
+                            <Link to='https://play.google.com/store/apps/details?id=com.farmerspp.com&hl=en_IN&gl=US&pli=1'><button className='md:px-[16px] md:py-[16px] border-4 font-semibold py-[4px] px-[8px] border-white rounded-full'>Download App</button></Link>
+                           <Link to='https://docs.google.com/forms/d/e/1FAIpQLSfPIP8JddDoDkeMFgXOWYv2eZq3yp68zM06i0JmScN32oqQ8w/viewform'> <button className='md:px-[49px] md:py-[16px] border-4 py-[4px] px-[20px] font-bold border-white text-black bg-white rounded-full flex gap-1'>Join Us</button></Link>
                         </div>
                       
                             <p className='mt-6 text-yellow-200 text-xl font-semibold'>200 Members already joined  farmsbook community</p>
--- src/component/WhatsJoin/WhatsJoin.jsx
+++ src/component/WhatsJoin/WhatsJoin.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import whatsapp from '../../assets/whats app.png';
+import { Link } from 'react-router-dom';
 
 const WhatsJoin = ({ title, head, alt, memberJoin }) => {
     return (
@@ -8,8 +9,8 @@ const WhatsJoin = ({ title, head, alt, memberJoin }) => {
                 {head && <p className='text-white text-[18px] mt-[40px]'>{head}</p>}
                 <p className='text-[24px] mb-[32px] md:mt-[30px] mt-5 text-center text-white'>{title}</p>
                 {memberJoin && <p className='mb-6 text-white'>{memberJoin}</p>}
-                {alt == true ? <button className='px-[24px] py-[8px] border-2 border-white text-black bg-white rounded flex gap-1'><img className='h-[24px] w-[23.92px]' src={whatsapp} alt="" />Join Us</button>
-                    : <button className='px-[24px] py-[8px] border-4 border-white text-white font-semibold  rounded flex gap-1'><img className='h-[24px] w-[23.92px]' src={whatsapp} alt="" />Join Us</button>}
+                {alt == true ? <Link to='https://docs.google.com/forms/d/e/1FAIpQLSfPIP8JddDoDkeMFgXOWYv2eZq3yp68zM06i0JmScN32oqQ8w/viewform'><button className='px-[24px] py-[8px] border-2 border-white text-black bg-white rounded flex gap-1'><img className='h-[24px] w-[23.92px]' src={whatsapp} alt="" />Join Us</button></Link>
+                    : <Link to='https://docs.google.com/forms/d/e/1FAIpQLSfPIP8JddDoDkeMFgXOWYv2eZq3yp68zM06i0JmScN32oqQ8w/viewform'><button className='px-[24px] py-[8px] border-4 border-white text-white font-semibold  rounded flex gap-1'><img className='h-[24px] w-[23.92px]' src={whatsapp} alt="" />Join Us</button></Link>}
             </div>
         </div>
     );
